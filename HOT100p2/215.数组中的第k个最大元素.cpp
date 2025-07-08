2d56b0cf582f619c20060d0229c66a58677ac3c9
Kize0 <kize0603@gmail.com>
2025-07-08 18:22:41 +0800

    [DAY 96] 2025-7-8

---
+++ HOT100p2/215.数组中的第k个最大元素.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=cpp
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+#pragma once
+#include <bits/stdc++.h>
+#include <ranges>
+using namespace std;
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+#define ALL(v) v.begin(), v.end()
+#define For(i, _) for (int i = 0, i##end = _; i < i##end; ++i)
+#define FOR(i, _, __) for (int i = _, i##end = __; i < i##end; ++i)
+#define Rep(i, _) for (int i = (_); i >= 0; --i)
+#define REP(i, __, _) for (int i = (__), i##end = _; i >= i##end; --i)
+typedef long long ll;
+typedef unsigned long long ull;
+#define V vector
+#define pb push_back
+#define pf push_front
+#define qb pop_back
+#define qf pop_front
+#define eb emplace_back
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+#define fi first
+#define se second
+const int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+const int inf = 0x3f3f3f3f;
+const ll infl = 0x3f3f3f3f3f3f3f3fll;
+const int mod = 1e9 + 7;
+template <class T>
+inline bool ckmin(T &x, const T &y) { return x > y ? (x = y, true) : false; }
+template <class T>
+inline bool ckmax(T &x, const T &y) { return x < y ? (x = y, true) : false; }
+int init = []()
+{ ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        auto foo = [&] (auto&& self, V<int>& nums, int k) -> int
+        {
+            int piv = nums[rand() % nums.size()];
+            V<int> l, r, m;
+            for (auto x : nums) {
+                if (x < piv) l.pb(x);
+                else if (x == piv) m.pb(x);
+                else r.pb(x);
+            }
+            if (r.size() >= k) return self(self, r, k);
+            else if (m.size() + r.size() < k) return self(self, l, k - m.size() - r.size());
+            else return piv;
+        };
+        return foo(foo, nums, k);
+    }
+};
+// @lc code=end
+
