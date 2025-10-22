bd89376218b69063c94cbd7c7382a1e052d99196
kxpzke-zen <kxpzke.zen@gmail.com>
2025-10-22 16:00:17 +0800

    [DAY 202] 2025-10-22

---
+++ 题单/&1/142.环形链表-ii.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=142 lang=python3
+#
+# [142] 环形链表 II
+#
+from mytools import *
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow: break
+        else: return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+# @lc code=end
+
